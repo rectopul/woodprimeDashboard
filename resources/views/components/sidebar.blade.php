eb6c436434178f6907b627974fe7138d8be74aa9
Rogério Bonfim <rogeriomateus@icloud.com>
2020-06-30 14:50:20 -0300

    product destroy

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -3,12 +3,10 @@
     <div class="list-group tabTypes" id="list-tab" role="tablist">
         <a class="list-group-item list-group-item-action active" id="v-pills-customization-tab" data-toggle="list" href="#v-pills-customization" role="tab" aria-controls="v-pills-customization">
             Personalização
-            <span class="badge badge-pill badge-primary">{{ $count }}</span>
         </a><!-- PERSONALIZAÇÃO // -->
 
         <a class="list-group-item list-group-item-action" id="v-pills-searchCustomization-tab" data-toggle="list" href="#v-pills-searchCustomization" role="tab" aria-controls="v-pills-searchCustomization">
             Buscar Personalização
-            <span class="badge badge-pill badge-primary pillcustoms">{{ $customizationsCount }}</span>
         </a><!-- busca de personalização // -->
 
         <a class="list-group-item list-group-item-action" id="v-pills-product-tab" data-toggle="list" href="#v-pills-product" role="tab" aria-controls="v-pills-product">
